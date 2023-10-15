428297b3647b3a170d6d767ba0bf2324a070519b
ademvnc <ademmavanaci@gmail.com>
2023-10-15 17:52:56 +0300

    PreLab1
    files were rearranged and added

---
+++ PreLab1/doc/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../_o_o_p_l_a_b1__22__23___pre_lab1_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'OOPLAB1_22_23_PreLab1.cpp']]]
+];
---
+++ PreLab1/doc/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ooplab1_5f22_5f23_5fprelab1_2ecpp_0',['OOPLAB1_22_23_PreLab1.cpp',['../_o_o_p_l_a_b1__22__23___pre_lab1_8cpp.html',1,'']]]
+];
